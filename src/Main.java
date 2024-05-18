9daf5df3213c88dcc8e9341101d91714c9e806ab
cyberpropper <osankin2002@mail.ru>
2024-05-19 01:10:00 +0700

    1задание

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
+        byte age = 27;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        }   else  {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
 
     }
 }
